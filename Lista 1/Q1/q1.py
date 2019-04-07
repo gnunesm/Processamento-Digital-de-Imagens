1b0f0631ac956a3070c99e98cc102637da325c01
gnunesm <gabnunes2112@gmail.com>
2019-04-07 16:08:09 -0300

    working version (no histogram yet)

--- Lista 1/Q1/q1.py
+++ Lista 1/Q1/q1.py
@@ -1,12 +1,15 @@
 import cv2
 import numpy as np
+import sys
+
+sys.setrecursionlimit(10000)
 
 img = cv2.imread('Fig8.02.jpg', 0)
 thresh = 159
 
 img[img > thresh] = 255
 img[img <= thresh] = 0
-cv2.imwrite('binary_sticks_.png', img)
+cv2.imwrite('binary_sticks.png', img)
 
 img_marker = np.zeros(img.shape)
 counts = []
@@ -18,19 +21,29 @@ def neighbor_marking(mark, line, column):
     for l in range(line - 1, line + 2):
         for c in range(column - 1, column + 2):
             if img[l][c] != 0:
-                if img_marker[m][n] == 0:
+                if img_marker[l][c] == 0:
+                    img_marker[l][c] = mark
+                    marked += 1
                     marked += neighbor_marking(mark, l, c)
     return marked
+
+print(img.shape)
+print(img_marker.shape)
 
 for m in range(img.shape[0]):
     for n in range(img.shape[1]):
         if img_marker[m][n] == 0:
             if img[m][n] != 0:
-                counts.append(neighbor_marking(current_stick, m, n))
+                img_marker[m][n] = current_stick
+                counts.append(1+neighbor_marking(current_stick, m, n))
                 current_stick += 1
 
 for idx, numb in enumerate(counts):
-    if numb in img_marker:
-        print('Palito {}: {} pixels'.format(idx + 1, numb))
+    print('Palito {}: {} pixels'.format(idx + 1, numb))
 
+img_new = np.zeros(img.shape)
 
+for n in range(1, 14):
+    img_new[img_marker == n] = 255/13*n
+
+cv2.imwrite('colored_sticks.png', img_new)
