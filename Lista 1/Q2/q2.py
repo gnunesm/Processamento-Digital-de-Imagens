07da3430a35c449b5b719cf6eda36e574a48b79a
gnunesm <gabnunes2112@gmail.com>
2019-04-07 16:51:13 -0300

    laplacian 3 working

--- Lista 1/Q2/q2.py
+++ Lista 1/Q2/q2.py
@@ -10,10 +10,10 @@ def filtering(mask, img):
 
 def convolution_lowpass(n, img):
     extension = n - 1
-    mask = np.ones((n, n), dtype=np.uint32)
-    extended_img = np.zeros((img.shape[0]+extension, img.shape[1]+extension), dtype=np.uint32)
+    mask = np.ones((n, n), dtype=np.int64)
+    extended_img = np.zeros((img.shape[0]+extension, img.shape[1]+extension), dtype=np.int64)
     extended_img[extension//2:-extension//2, extension//2:-extension//2] = img
-    filtered_img = np.zeros(img.shape, dtype=np.uint32)
+    filtered_img = np.zeros(img.shape, dtype=np.int64)
     for l in range(img.shape[0]):
         for c in range(img.shape[1]):
             filtered_img[l][c] = filtering(mask, extended_img[l:l+n, c:c+n])
@@ -25,10 +25,11 @@ def convolution_laplacian(n, img):
     extension = n - 1
     mask = np.array([[0, -1, 0],
                      [-1, 4, -1],
-                     [0, -1, 0]], np.uint32)
-    extended_img = np.zeros((img.shape[0]+extension, img.shape[1]+extension), dtype=np.uint32)
+                     [0, -1, 0]], np.int64)
+    # mask = mask * -1
+    extended_img = np.zeros((img.shape[0]+extension, img.shape[1]+extension), dtype=np.int64)
     extended_img[extension//2:-extension//2, extension//2:-extension//2] = img
-    filtered_img = np.zeros(img.shape, dtype=np.uint32)
+    filtered_img = np.zeros(img.shape, dtype=np.int64)
     for l in range(img.shape[0]):
         for c in range(img.shape[1]):
             filtered_img[l][c] = filtering(mask, extended_img[l:l+n, c:c+n])
@@ -38,9 +39,11 @@ def convolution_laplacian(n, img):
     return filtered_img
 
 img = cv2.imread('lena.tif', 0)
-img = img.astype(np.uint32)
+img = img.astype(np.int64)
 for n in range(3, 17, 2):
-    filtered_img = convolution_lowpass(n, img)
-    cv2.imwrite('lowpass_{}.png'.format(n), filtered_img)
-    # filtered_img = convolution_laplacian(n, img)
-    # cv2.imwrite('laplacian_{}.png'.format(n), filtered_img)
+    # filtered_img = convolution_lowpass(n, img)
+    # cv2.imwrite('lowpass_{}.png'.format(n), filtered_img)
+
+n = 3
+filtered_img = convolution_laplacian(n, img)
+cv2.imwrite('laplacian_{}.png'.format(n), filtered_img)
