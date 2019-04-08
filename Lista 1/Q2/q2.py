bb90df0483182d8318974f2dfec9e5320d4eb853
gnunesm <gabnunes2112@gmail.com>
2019-04-08 16:12:20 -0300

    no division on laplacian

--- Lista 1/Q2/q2.py
+++ Lista 1/Q2/q2.py
@@ -33,7 +33,7 @@ def convolution_laplacian(n, img):
     for l in range(img.shape[0]):
         for c in range(img.shape[1]):
             filtered_img[l][c] = filtering(mask, extended_img[l:l+n, c:c+n])
-    filtered_img = filtered_img/(n*n)
+    # filtered_img = filtered_img/(n*n)
     filtered_img = img + filtered_img
     filtered_img = filtered_img.astype(np.uint8)
     return filtered_img
@@ -41,8 +41,8 @@ def convolution_laplacian(n, img):
 img = cv2.imread('lena.tif', 0)
 img = img.astype(np.int64)
 for n in range(3, 17, 2):
-    # filtered_img = convolution_lowpass(n, img)
-    # cv2.imwrite('lowpass_{}.png'.format(n), filtered_img)
+    filtered_img = convolution_lowpass(n, img)
+    cv2.imwrite('lowpass_{}.png'.format(n), filtered_img)
 
 n = 3
 filtered_img = convolution_laplacian(n, img)
