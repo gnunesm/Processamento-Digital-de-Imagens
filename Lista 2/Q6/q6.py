7350bd468729b09961b0f6303da05800fb66ca5e
gnunesm <gabnunes2112@gmail.com>
2019-06-12 11:33:46 -0300

    mod_erosion (failed attempt)

--- Lista 2/Q6/q6.py
+++ Lista 2/Q6/q6.py
@@ -2,6 +2,18 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
+def mod_erosion(img, n):
+    height = img.shape[0]
+    width = img.shape[1]
+    eroded = np.zeros(img.shape)
+    pad = n//2
+    eroded[pad:-1*pad, pad:-1*pad] = 255*np.ones([img.shape[0]-2*(pad), img.shape[1]-2*(pad)])
+    for l in range(pad, height-pad):
+        for c in range(pad, width-pad):
+            if not np.all(img[l-pad:l+pad+1, c-pad:c+pad+1]==255):
+                eroded[l][c] = 0
+    return eroded
+
 img = cv2.imread('Fig10.40(a).jpg', 0)
 
 # pixel_values = [0]*256
@@ -17,3 +29,6 @@ thresh = 254
 img[img > thresh] = 255
 img[img <= thresh] = 0
 cv2.imwrite('3-binary.png', img)
+
+img = mod_erosion(img, 3)
+cv2.imwrite('eroded.png', img)
