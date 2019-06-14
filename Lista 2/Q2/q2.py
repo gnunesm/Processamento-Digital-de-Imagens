0bc9b5e9e737365968bbc2bc3fb3130afd127d8a
gnunesm <gabnunes2112@gmail.com>
2019-06-14 16:10:30 -0300

    RGB2HSI done

--- Lista 2/Q2/q2.py
+++ Lista 2/Q2/q2.py
@@ -9,16 +9,24 @@ img = img.astype(np.float64)
 
 # RGB to HSI
 hsi_img = np.zeros(img.shape, np.float64)
-theta = np.zeros(img.shape, np.float64)
-theta[] = np.arccos(0.5*(img[:,:,2]-img[:,:,1]+img[:,:,2]-img[:,:,0])/np.sqrt((img[:,:,2]-img[:,:,1])**2+(img[:,:,2]-img[:,:,0])*(img[:,:,1]-img[:,:,0])))
+theta = np.zeros(img.shape[:2], np.float64)
+# mask = img.sum(axis=2)!=0
+# print(theta.shape, theta[mask].shape)
+# theta[] = np.arccos(0.5*(img[mask][:,:,2]-img[mask][:,:,1]+img[mask][:,:,2]-img[mask][:,:,0])/np.sqrt((img[mask][:,:,2]-img[mask][:,:,1])**2+(img[mask][:,:,2]-img[mask][:,:,0])*(img[mask][:,:,1]-img[mask][:,:,0])))
+
+for l in range(theta.shape[0]):
+    for c in range(theta.shape[1]):
+        if img[l][c].sum() != 0:
+            theta[l][c] = np.arccos(0.5*(img[l,c,2]-img[l,c,1]+img[l,c,2]-img[l,c,0])/np.sqrt((img[l,c,2]-img[l,c,1])**2+(img[l,c,2]-img[l,c,0])*(img[l,c,1]-img[l,c,0])))
 
 for l in range(img.shape[0]):
     for c in range(img.shape[1]):
-        if img[l][c][0] <= img[l][c][2]:
-            hsi_img[l][c][0] = theta[l][c]
-        else:
-            hsi_img[l][c][0] = 2*np.pi - theta[l][c]
-        hsi_img[l][c][1] = 1 - 3*img[l][c].min()/(img[l][c][2]+img[l][c][1]+img[l][c][0])
+        if img[l][c].sum() != 0:
+            if img[l][c][0] <= img[l][c][2]:
+                hsi_img[l][c][0] = theta[l][c]
+            else:
+                hsi_img[l][c][0] = 2*np.pi - theta[l][c]            
+            hsi_img[l][c][1] = 1 - 3*img[l][c].min()/(img[l][c][2]+img[l][c][1]+img[l][c][0])
 
 hsi_img[:,:,2] = (img[:,:,2] + img[:,:,1] + img[:,:,0])/3
 
