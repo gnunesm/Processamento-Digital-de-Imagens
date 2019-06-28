45ba2b2c0ae816da9f670330cb4ff734fb16d81d
gnunesm <gabnunes2112@gmail.com>
2019-06-28 17:42:01 -0300

    updates

--- Lista 2/Q4/q4.py
+++ Lista 2/Q4/q4.py
@@ -4,30 +4,28 @@ import numpy as np
 
 thresh = 20
 
+# leitura da imagem
 img = cv2.imread('pontos.bmp', 0)
-(height, width) = img.shape
+height, width = img.shape
 
+# mapeamento dos pontos
 points = []
 for l in range(height):
     for c in range(width):
         if img[l][c] > 127:
             points.append((c, height-1-l))
-points.sort(key=lambda t: t[0])
+points.sort(key=lambda t: t[0]) # ordenação de acordo com coordenada x (esquerda pra direita)
 
+# ordenação dos pontos em sentido horário
 ordered = []
 while points:
     ordered.append(points.pop(0))
-    # img[height-ordered[-1][1]-1][ordered[-1][0]] = 0
-    # cv2.imwrite('img{}.png'.format(len(ordered)), img)
     points.sort(key=lambda t: (t[0] - ordered[-1][0])**2 + (t[1] - ordered[-1][1])**2)
-# ordered.append(ordered[0])
 
 ordered = np.array(ordered)
 
 start_point = ordered[0]
 end_point = max(ordered, key=lambda t: t[0])
-
-# ordered = np.delete(ordered, ordered.index())
 
 closed_list = [end_point]
 open_list = [end_point, start_point]
@@ -39,9 +37,7 @@ while open_list:
         elif (ordered[n] == closed_list[-1]).all():
             end_idx = n
     if start_idx+1 < end_idx:
-        # print(open_list)
         d = np.absolute(np.cross(open_list[-1]-closed_list[-1],ordered[start_idx+1:end_idx]-closed_list[-1])/np.linalg.norm(open_list[-1]-closed_list[-1]))
-        # print(d)
         if max(d) >= thresh:
             idx = np.argmax(d) + start_idx + 1
             open_list.append(ordered[idx])
@@ -49,16 +45,11 @@ while open_list:
             closed_list.append(open_list.pop())
     else:
         closed_list.append(open_list.pop())
-    # print(open_list)
 closed_list.pop() # impede de fechar o loop
-# descobrir se precisa usar o método do livro (se precisa ligar todos e se pode usar minha ordenação já feita)
 
-# print(closed_list)
 for p in ordered:
     plt.plot(p[0], p[1], 'go')
 
-# print(closed_list)
-# plt.plot(closed_list[0], closed_list[1], 'r-')
 for n in range(len(closed_list)-1):
     plt.plot((closed_list[n][0], closed_list[n+1][0]), (closed_list[n][1], closed_list[n+1][1]), 'r-')
 
@@ -68,8 +59,6 @@ ordered = np.delete(ordered, 0, axis=0)
 start_point = max(ordered, key=lambda t: t[0])
 end_point = min(ordered, key=lambda t: t[0])
 
-# ordered = np.delete(ordered, ordered.index())
-
 closed_list = [end_point]
 open_list = [end_point, start_point]
 
@@ -80,9 +69,7 @@ while open_list:
         elif (ordered[n] == closed_list[-1]).all():
             end_idx = n
     if start_idx+1 < end_idx:
-        # print(open_list)
         d = np.absolute(np.cross(open_list[-1]-closed_list[-1],ordered[start_idx+1:end_idx]-closed_list[-1])/np.linalg.norm(open_list[-1]-closed_list[-1]))
-        # print(d)
         if max(d) >= thresh:
             idx = np.argmax(d) + start_idx + 1
             open_list.append(ordered[idx])
@@ -90,19 +77,9 @@ while open_list:
             closed_list.append(open_list.pop())
     else:
         closed_list.append(open_list.pop())
-    # print(open_list)
 closed_list.pop() # impede de fechar o loop
-# descobrir se precisa usar o método do livro (se precisa ligar todos e se pode usar minha ordenação já feita)
 
-# print(closed_list)
-# plt.plot(closed_list[0], closed_list[1], 'r-')
 for n in range(len(closed_list)-1):
     plt.plot((closed_list[n][0], closed_list[n+1][0]), (closed_list[n][1], closed_list[n+1][1]), 'r-')
 
 plt.show()
-
-
-
-
-
-    
